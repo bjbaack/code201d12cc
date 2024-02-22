8af4a9964e79791c4d35cd81052c448047494a9e
bradbaack <bjbaack@gmail.com>
2024-02-22 13:37:21 -0600

    double paste

--- hellowworld.sh
+++ hellowworld.sh
@@ -17,25 +17,3 @@
 
 
 # End
-	
-#!/bin/bash
-
-# Script Name:                  Hello World
-# Author:                       Brad Baack
-# Date of latest revision:      02/20/2024
-# Purpose:                      Print a stirng to the terminal
-
-
-echo "Hello World"
-# Declaration of variables
-greeting="I am working perfectly"
-
-# Declaration of functions
-
-
-# Main
-echo $greeting
-
-
-
-# End
