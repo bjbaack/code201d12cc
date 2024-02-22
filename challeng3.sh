f554a2969f66ff32ecc618ce84d91c92ce2005d3
bradbaack <bjbaack@gmail.com>
2024-02-22 13:05:00 -0600

    changed repostiory name

---
+++ challeng3.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+
+# Script Name:                  Print Login History
+# Author:                       Brad Baack
+# Date of latest revision:      02/21/2024
+# Purpose:                      Print login history
+
+# Resources
+# https://ryanstutorials.net/bash-scripting-tutorial/bash-functions.php
+
+# Declaration of variables
+
+
+# Declaration of functions
+print_login_history() {
+last
+echo "This is the login history"
+}
+
+# Main
+
+print_login_history
+
+print_login_history
+
+print_login_history
+
+# End
---
+++ hellowworld.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Brad Baack
+# Date of latest revision:      02/20/2024
+# Purpose:                      Print a stirng to the terminal
+
+
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+# Main
+
+
+# End
+	
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Brad Baack
+# Date of latest revision:      02/20/2024
+# Purpose:                      Print a stirng to the terminal
+
+
+echo "Hello World"
+# Declaration of variables
+greeting="I am working perfectly"
+
+# Declaration of functions
+
+
+# Main
+echo $greeting
+
+
+
+# End
